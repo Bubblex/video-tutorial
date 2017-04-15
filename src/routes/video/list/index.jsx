bc054c99d1b310ac0c5a7d242295d4fd833e3069
江肖 <407505475@qq.com>
2017-04-15 18:24:35 +0800

    修改roadhogrc配置

--- src/routes/video/list/index.jsx
+++ src/routes/video/list/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { connect } from 'dva'
+// import { Layout, Header, Content, Footer } from 'antd'
 
 class VideoList extends React.Component {
   render() {
     return (
-      <div>视频列表</div>
+      <div>
+        视频列表
+        {/* <Layout>
+          <Header>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>*/}
+      </div>
     )
   }
 }
