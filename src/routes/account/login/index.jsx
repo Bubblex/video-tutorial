e42d6fb57c06bd11a60e9d125bd57c4121734660
江肖 <407505475@qq.com>
2017-04-20 23:30:17 +0800

    登录

--- src/routes/account/login/index.jsx
+++ src/routes/account/login/index.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import { connect } from 'dva'
 
+import {
+  Card,
+  Input,
+  Button,
+} from 'antd'
+
+import BackGroungImg from '../../../assets/account-bg.jpg'
+import LOGO from '../../../assets/logo.png'
+
+import styles from './index.less'
+
 class Login extends React.Component {
   render() {
     return (
-      <div>登录</div>
+      <div className={styles.login}>
+        <img src={BackGroungImg} className={styles.bg} alt='背景图' />
+        <Card className={styles.container}>
+          <img src={LOGO} className={styles.logo} alt='背景图' />
+          <Input placeholder='账号' size='large' className={styles.input} />
+          <Input placeholder='账号' size='large' className={styles.input} />
+          <Button size='large' style={{ width: '80%', marginTop: '20px' }}>登录</Button>
+          <Button type='primary' size='large' style={{ width: '80%', marginTop: '10px' }}>注册</Button>
+        </Card>
+      </div>
     )
   }
 }
