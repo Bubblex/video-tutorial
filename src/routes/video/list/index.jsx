1ab5dd7ed3b3e88b9a98eb9358134ad053ac41f8
江肖 <407505475@qq.com>
2017-05-09 14:56:00 +0800

    列表加收藏数

--- src/routes/video/list/index.jsx
+++ src/routes/video/list/index.jsx
@@ -86,25 +86,28 @@ class VideoList extends React.Component {
             <Col span={5}>
               <img src={arr.cover} alt={arr.title} style={{ width: '100%' }} />
             </Col>
-            <Col span={16} offset={1}>
-              <h2>
-                {arr.title}
-              </h2>
+            <Col span={18} offset={1}>
+              <Row>
+                <Col span={20}><h2>{arr.title}</h2></Col>
+                <Col span={4}>
+                  <Link to='/video/detail' query={{ id: arr.id }}>查看详情</Link>
+                </Col>
+              </Row>
               <p style={{ margin: '10px 0' }}>{arr.summary}</p>
-              <Col span={10}>
-                <p>作者：{arr.author.nickname}</p>
-              </Col>
-              <Col span={12}>
-                <p>发布时间：{arr.created_at}</p>
-              </Col>
-              <Col span={2}><p><span>{arr.play_num}</span> <Icon type='eye-o' /></p></Col>
-            </Col>
-            {/* <Col span={2}><p><span>{arr.like_num}</span> <Icon type='heart-o' /> </p></Col>
-            <Col span={2}><p><span>{arr.comment_num}</span> <Icon type='message' /> </p></Col>*/}
-            <Col span={2}>
-              <Link to='/video/detail' query={{ id: arr.id }}>
-                查看详情
-              </Link>
+              <Row>
+                <Col span={7}>
+                  <p>作者：{arr.author.nickname}</p>
+                </Col>
+                <Col span={11}>
+                  <p>发布时间：{arr.created_at}</p>
+                </Col>
+                <Col span={3}>
+                  <p><span>{arr.play_num}</span> <Icon type='eye-o' /></p>
+                </Col>
+                <Col span={3}>
+                  <p><span>{arr.collects_count}</span> <Icon type='heart-o' /></p>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Card>
