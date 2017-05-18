f135e4742b9fcf2a651b43bcf8c3e73a7ba4432e
江肖 <407505475@qq.com>
2017-05-18 15:13:37 +0800

    处理iconfont

--- src/config/theme.js
+++ src/config/theme.js
@@ -8,5 +8,5 @@ module.exports = {
   '@layout-footer-padding': '12px 24px',
   '@primary-color': '#a3bd1d',
   '@layout-header-background': '#000',
-  '@icon-url': '/assets/fonts',
+  '@icon-url': '"/fonts/iconfont"'
 }
